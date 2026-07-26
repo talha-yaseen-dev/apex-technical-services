dcfd7bb59136f6f12d2132d0c37bdd4139e54628
Talha Yaseen <talhayaseen176@gmail.com>
2026-07-26 21:53:55 +0500

    Deploy deep-teal redesign (peak wordmark, collapsed mobile menu, navbar feedback)

---
+++ _next/static/chunks/app/(ar)/ar/services/page-5d2bd7c799c17f5b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[310,852,706,77,690,238,279,253,583,153],{256:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=256)}),_N_E=n.O()}]);
